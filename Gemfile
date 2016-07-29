c4cc6f8282a468110af965bf10f5dfb7dba80321
Samuel <sbellon2019@gmail.com>
2016-07-29 19:11:11 +0000

    images config

--- Gemfile
+++ Gemfile
@@ -15,10 +15,8 @@ gem 'bootstrap-sass', '~> 3.3.6'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 #images
-gem 'paperclip', :git=> 'https://github.com/thoughtbot/paperclip', :ref => '523bd46c768226893f23889079a7aa9c73b57d68'
-
-#Amazon Web Services Cloud Storage
-gem 'aws-sdk', '< 2.0'
+gem 'paperclip'
+gem 'aws-sdk', '~> 2.3'
 
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,11 +1,13 @@
 class Topic < ActiveRecord::Base
-    has_many :votes, dependent: :destroy
-    
+     has_many :votes, dependent: :destroy
     has_attached_file :image, styles: {
-     :medium => '640x',
-     :small =>'400x'
-     }
+      thumb: '100x100>',
+      square: '200x200#',
+      medium: '300x300>'
+     } 
+     #:url => '/public/:attachment/000/000/:id/:style_:basename.:extension'
 
- validates_attachment_content_type :image, :content_type => :image, :content_type => /\Aimage\/.*\Z/
-    
+
+    # Validate the attached image is image/jpg, image/png, etc
+    validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
